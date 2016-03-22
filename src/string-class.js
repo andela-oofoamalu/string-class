e6ddf12c6ebda0a371b0b931061fc5db7a7e3b71
andela-oofoamalu <jessicaprincess61@gmail.com>
2016-03-22 20:53:38 +0100

    Add functionality to methods

--- src/string-class.js
+++ src/string-class.js
@@ -1,23 +1,48 @@
 'use strict';
 
 function MyStringClass (arg) {
-  this.value = arg;
+  this.string = arg;
 }
 
-MyStringClass.prototype = new String();
-MyStringClass.prototype.toString = MyStringClass.prototype.valueOf = function(){return this.value};
+MyStringClass.prototype = new String;
+MyStringClass.prototype.toString = MyStringClass.prototype.valueOf = function() {return this.value};
 MyStringClass.prototype.constructor = String;
 
 MyStringClass.prototype.hasVowels = function() {
-  return false;
+  var expression = /[aeiou]/g;
+  return expression.test(this.string);
 };
 
 MyStringClass.prototype.toUpper = function() {
-  return null;
+  var upperCaseString = "";
+
+  for (var i = 0; i < this.string.length; i++) {
+    var char = this.string.charAt(i);
+
+    if (isNaN(char) && !/[A-Z]/.test(char)) {
+      var upperCaseCode = this.string.charCodeAt(i) - 32;
+      upperCaseString += String.fromCharCode(upperCaseCode);
+    } else {
+      upperCaseString += char;
+    }
+  }
+  return upperCaseString;
 };
 
 MyStringClass.prototype.toLower = function() {
-  return null;
+  var lowerCaseString = "";
+
+  for (var i = 0; i < this.string.length; i++) {
+    var char = this.string.charAt(i);
+
+    if (isNaN(char) && /[A-Z]/.test(char)) {
+      var lowerCaseCode = this.string.charCodeAt(i) + 32;
+      lowerCaseString += String.fromCharCode(lowerCaseCode);
+    } else {
+      lowerCaseString += char;
+    }
+  }
+  return lowerCaseString;
 };
 
 MyStringClass.prototype.ucFirst = function() {
@@ -25,15 +50,18 @@ MyStringClass.prototype.ucFirst = function() {
 };
 
 MyStringClass.prototype.isQuestion = function() {
-  return false;
+  var expression = /.\?$/;
+  return expression.test(this.string);
 };
 
 MyStringClass.prototype.words = function() {
-  return [];
+  var expression = /(\w)+./g;
+  return this.string.match(expression);
 };
 
 MyStringClass.prototype.wordCount = function() {
-  return 0;
+  var wordArray = this.words();
+  return wordArray.length;
 };
 
 MyStringClass.prototype.toCurrency = function() {
