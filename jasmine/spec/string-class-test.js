415befcba52eea72ba6c3726e7a08139906cfd65
andela-oofoamalu <jessicaprincess61@gmail.com>
2016-03-22 16:32:19 +0100

    Write tests for method hasVowels()

--- jasmine/spec/string-class-test.js
+++ jasmine/spec/string-class-test.js
@@ -2,13 +2,46 @@
 
 describe("MyStringClass test", function() {
 
-  describe("Has vowels", function() {
-    it("Should return true if a vowel exists in the string, or false if otherwise", function() {
+  describe("pre-tests", function() {
+    it("Should check the existence of defined methods in MyStringClass", function() {
+      var myString = new MyStringClass();
 
+      expect(MyStringClass).toBeDefined();
+      expect(myString.hasVowels).toBeDefined();
+      expect(myString.toUpper).toBeDefined();
+      expect(myString.toLower).toBeDefined();
+      expect(myString.ucFirst).toBeDefined();
+      expect(myString.isQuestion).toBeDefined();
+      expect(myString.words).toBeDefined();
+      expect(myString.wordCount).toBeDefined();
+      expect(myString.toCurrency).toBeDefined();
+      expect(myString.fromCurrency).toBeDefined();
+    });
+  });
+
+  describe("Has vowels", function() {
+    var string1, string2;
+
+    beforeEach(function() {
+      string1 = new MyStringClass("Hello World");
+      string2 = new MyStringClass("jpppp");
+    });
+
+    it("Should return true if a vowel exists in the string, or false if otherwise", function() {
+      var result1 = string1.hasVowels();
+      var result2 = string2.hasVowels();
+
+      expect(result1).toEqual(true);
+      expect(result2).toEqual(false);
     });
 
     it("Should be called without passing an argument", function() {
+      spyOn(string1, 'hasVowels');
+      string1.hasVowels();
 
+      expect(string1.hasVowels).toHaveBeenCalled();
+      expect(string1.hasVowels).toHaveBeenCalledWith();
+      expect(string1.hasVowels).not.toHaveBeenCalledWith("Hello World");
     });
   });
 
